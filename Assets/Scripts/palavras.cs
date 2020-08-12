13b265137edf311b60da60ff12cc15cb972db415
João Pedro Matias <jpmmatias@gmail.com>
2020-08-12 19:56:00 -0300

    FinishedGame

---
+++ Assets/Scripts/palavras.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class palavras : MonoBehaviour
+{
+    private List<string> palavrasOriginais = new List<string>()
+    {
+        "Eu","Gosto","De","Sorvete"
+    };
+
+    private List<string> palavrasJogo = new List<string>();
+
+    private void Awake()
+    {
+        palavrasJogo.AddRange(palavrasOriginais);
+        Shuffle(palavrasJogo);
+        ConverterMinusculo(palavrasJogo);
+    }
+
+    private void Shuffle(List<string> list)
+    {
+        for(int i = 0; i < list.Count; i++)
+        {
+            int random = Random.Range(i, list.Count);
+            string temporario = list[i];
+
+            list[i] = list[random];
+            list[random] = temporario;
+        }
+    }
+
+    private void ConverterMinusculo(List<string> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            list[i] = list[i].ToLower();
+        }
+    }
+
+    public string PegarPalavra()
+    {
+        string palavraNova = string.Empty;
+        if(palavrasJogo.Count != 0)
+        {
+            palavraNova = palavrasJogo.Last();
+            palavrasJogo.Remove(palavraNova);
+        }
+        if(palavrasJogo.Count < 0)
+        {
+            return string.Empty;
+        }
+        return palavraNova;
+    }
+
+}
---
+++ Assets/Scripts/typer.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class typer : MonoBehaviour
+{
+    public palavras palavras = null;
+    public Text palavraOutput = null;
+    public GameObject GameCanvas;
+    public GameObject GameOverCanvas;
+
+    private string palavraRestante = string.Empty;
+    private string palavraAtual = string.Empty;
+    
+    private void Start()
+    {
+        SetPalavraAtual();
+    }
+
+    private void SetPalavraAtual()
+    {
+        palavraAtual = palavras.PegarPalavra();
+        if (palavraAtual != string.Empty)
+        {
+            SetPalavraRestante(palavraAtual);
+
+        }
+        else
+        {
+            GameCanvas.SetActive(false);
+            GameOverCanvas.SetActive(true);
+        }
+        
+
+    }
+
+    private void SetPalavraRestante(string newString)
+    {
+        palavraRestante = newString;
+        palavraOutput.text = palavraRestante;
+
+    }
+  
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.anyKeyDown)
+        {
+            string keysPressed = Input.inputString;
+
+            if (keysPressed.Length == 1)
+            {
+                MostrarLetra(keysPressed);
+            }
+        }
+
+    }
+
+    private void MostrarLetra(string letra)
+    {
+        if (eLetraCorreta(letra))
+        {
+            RemoverLetra();
+
+            if (ePalavraCompleta())
+            {
+                SetPalavraAtual();
+            }
+        }
+
+    }
+
+    private bool eLetraCorreta(string letra)
+    {
+
+        return palavraRestante.IndexOf(letra) == 0;
+    }
+
+    private void RemoverLetra()
+    {
+        string newString = palavraRestante.Remove(0, 1);
+        SetPalavraRestante(newString);
+    }
+
+    private bool ePalavraCompleta()
+    {
+        return palavraRestante.Length==0;
+    }
+}
